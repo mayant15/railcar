b4ccfadf47c400c38e8e9ee17dc0820791a13d86
Mayant Mukul <mayantmukul@outlook.in>
2025-09-13 15:08:13 -0700

    feat(cli): enable graph mode by default

--- packages/cli/src/main.rs
+++ packages/cli/src/main.rs
@@ -56,7 +56,7 @@ struct Arguments {
     crashes: String,
 
     /// Fuzz driver variant to use
-    #[arg(long, value_enum, default_value_t = FuzzerMode::Bytes)]
+    #[arg(long, value_enum, default_value_t = FuzzerMode::Graph)]
     mode: FuzzerMode,
 
     /// Port to spawn the IPC broker on. If spawning multiple instances they should have different
