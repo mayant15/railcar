84f3e97f8e0732f0c7ed41fde825acdd446cb056
Mayant Mukul <mayantm@proton.me>
2025-09-20 10:16:36 -0700

    feat(examples): add promise-polyfill

--- examples/promise-polyfill/railcar.config.js
+++ examples/promise-polyfill/railcar.config.js
@@ -1,6 +1,6 @@
 const { makeInvalidErrorMessageOracle } = require("@railcar/support");
 
 module.exports = {
-    instrumentFilter: (f) => f.includes("redux"),
+    instrumentFilter: (f) => f.includes("promise-polyfill"),
     oracle: makeInvalidErrorMessageOracle([]),
 };
