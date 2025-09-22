75fb9b59efeea66c06376ce9d6d165a257b2e00b
Mayant Mukul <mayantm@proton.me>
2025-09-22 09:57:35 -0700

    feat(examples): add lit

---
+++ examples/lit/railcar.config.js
@@ -0,0 +1,8 @@
+const { makeInvalidErrorMessageOracle } = require("@railcar/support");
+
+module.exports = {
+    instrumentFilter: (f) => f.includes("lit"),
+    oracle: makeInvalidErrorMessageOracle([
+        "Cannot read properties"
+    ]),
+};
