9bd2a0ea6148c7ab329a547375180e13df2bfcea
Mayant Mukul <mayantm@proton.me>
2025-09-22 10:05:21 -0700

    feat(examples): add google-closure-library

---
+++ examples/closure-library/railcar.config.js
@@ -0,0 +1,8 @@
+const { makeInvalidErrorMessageOracle } = require("@railcar/support");
+
+module.exports = {
+    instrumentFilter: (f) => f.includes("google-closure-library"),
+    oracle: makeInvalidErrorMessageOracle([
+        "Cannot read properties of"
+    ]),
+};
