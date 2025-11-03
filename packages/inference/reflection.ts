b8e012e7de453ef891d4fe3e38ed0a1d57d53147
Mayant Mukul <mayantm@proton.me>
2025-11-03 11:45:01 -0800

    fix: register require hooks for graph mode

--- packages/inference/reflection.ts
+++ packages/inference/reflection.ts
@@ -367,7 +367,9 @@ export async function loadSchema(
     schemaFile?: string,
     methodsToSkip?: EndpointName[],
 ): Promise<{ schema: Schema; endpoints: Endpoints }> {
-    const main = await import(mainModule);
+    // there seems to be issues setting up require hooks with Istanbul
+    // for `await import()`. Use `require()` instead.
+    const main = require(mainModule);
 
     const schema: Schema = schemaFile
         ? JSON.parse(readFileSync(schemaFile).toString())
