072c97f3ac7ebd10e7d06d5ffe960421bd25e468
Mayant Mukul <mayantm@proton.me>
2025-11-14 10:21:20 -0800

    feat(examples): remove fastify

--- examples/fastify/railcar.config.js
+++
@@ -1,6 +0,0 @@
-const { makeInvalidErrorMessageOracle } = require("@railcar/support");
-
-module.exports = {
-    instrumentFilter: (f) => f.includes("fastify"),
-    oracle: makeInvalidErrorMessageOracle([]),
-};
