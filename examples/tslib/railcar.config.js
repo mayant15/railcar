4d3c6c13f391f33b0520faa7cd601fc47918ac58
Mayant Mukul <mayantm@proton.me>
2025-12-14 19:42:06 -0800

    feat(examples): skip __read for tslib

--- examples/tslib/railcar.config.js
+++ examples/tslib/railcar.config.js
@@ -3,4 +3,5 @@ const { makeInvalidErrorMessageOracle } = require("@railcar/support");
 module.exports = {
     shouldInstrument: (f) => f.includes("tslib"),
     isBug: makeInvalidErrorMessageOracle(["Class extends value"]),
+    skipMethods: ["__read"],
 };
