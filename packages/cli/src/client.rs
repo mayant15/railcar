mayant15/railcar

commit 0b3329c1b1c91140e4d42dcf1b9ecc33ccb20fb6

feat: restart node process if IPC fails
there's supposed to be some shmem cleanup whenever the node.js process
crashes (like unhandled async exceptions in sharp). to avoid having to
handle all the concurrency edge cases, just kill and restart the node
process if any IPC fails during input execution.

packages/cli/src/client.rs
@@ -71,17 +71,9 @@ where
             return ExitKind::Ok;
         };
 
-        let code = match args.worker.invoke(&bytes) {
-            Ok(code) => code,
-            Err(e) => panic!("failed to invoke worker {}", e),
-        };
-
-        // From worker/common.ts
-        match code {
-            0 | 1 => ExitKind::Ok,
-            2 => ExitKind::Crash,
-            _ => unreachable!(),
-        }
+        args.worker
+            .invoke(&bytes)
+            .unwrap_or_else(|e| panic!("failed to invoke worker {}", e))
     };
 
     let mut executor = InProcessExecutor::with_timeout(

packages/fuzzer/src/seq/mod.rs
@@ -97,17 +97,9 @@ fn client(
             return ExitKind::Ok;
         };
 
-        let code = match worker.invoke(&bytes) {
-            Ok(code) => code,
-            Err(e) => panic!("failed to invoke worker {}", e),
-        };
-
-        // From worker/common.ts
-        match code {
-            0 | 1 => ExitKind::Ok,
-            2 => ExitKind::Crash,
-            _ => unreachable!(),
-        }
+        worker
+            .invoke(&bytes)
+            .unwrap_or_else(|e| panic!("failed to invoke worker {}", e))
     };
 
     let mut executor = InProcessExecutor::with_timeout(

packages/fuzzer/src/worker.rs
@@ -11,6 +11,7 @@ use std::{
 };
 
 use anyhow::{bail, Result};
+use libafl::executors::ExitKind;
 use libafl_bolts::shmem::{ShMem, ShMemDescription, ShMemProvider, StdShMem, StdShMemProvider};
 use nix::{
     sys::wait::WaitStatus,
@@ -47,15 +48,13 @@ pub struct WorkerArgs {
     pub config_file: Option<PathBuf>,
 }
 
-type ExitCode = u8;
-
 // NOTE: Keep in sync with worker/worker.ts
 #[derive(Serialize, Deserialize, Debug)]
 pub enum Message {
     Init(InitArgs),
     InitOk(Option<Schema>),
     Invoke(InvokeArgs),
-    InvokeOk(ExitCode),
+    InvokeOk(bool),
     Log(String),
     Terminate,
 }
@@ -119,67 +118,68 @@ impl Child {
         ));
         Ok(status)
     }
+
+    /// Spawn a NodeJS subprocess to run the target
+    ///
+    /// In case the fuzzer exits (crash or timeout), the default LibAFL signal handler calls
+    /// `libc::exit`. When simply invoked with a `Command::spawn`, this didn't close the child
+    /// process. Set a death signal with `set_pdeathsig` so that we send a SIGKILL to the child
+    /// process when the fuzzer process terminates for any reason. This also pipes stdout and
+    /// stdin of the child process for IPC.
+    fn spawn(opts: SpawnNodeChildOptions) -> Result<Child> {
+        use nix::unistd;
+
+        let worker_script = find_worker_script()?;
+
+        let (in_read, in_write) = unistd::pipe()?;
+        let (out_read, out_write) = unistd::pipe()?;
+        let result = unsafe { unistd::fork() }?;
+        match result {
+            ForkResult::Parent { child } => {
+                drop(in_read);
+                drop(out_write);
+
+                Ok(Child {
+                    pid: child,
+                    stdout: PipeReader(out_read),
+                    stdin: PipeWriter(in_write),
+                })
+            }
+            ForkResult::Child => {
+                use nix::sys::{prctl, signal::Signal};
+
+                prctl::set_pdeathsig(Signal::SIGKILL).expect("failed to set death signal");
+
+                drop(in_write);
+                drop(out_read);
+
+                let stderr = if opts.discard_stderr {
+                    Stdio::null()
+                } else {
+                    Stdio::inherit()
+                };
+
+                let err = Command::new("node")
+                    .arg(worker_script)
+                    .stdin(Stdio::from(in_read))
+                    .stdout(Stdio::from(out_write))
+                    .stderr(stderr)
+                    .exec();
+                panic!("failed to spawn node subprocess: {}", err);
+            }
+        }
+    }
 }
 
 pub struct Worker {
     proc: Child,
     schema: Option<Schema>,
     shmem: Option<StdShMem>,
+    args: WorkerArgs,
 }
 
 struct SpawnNodeChildOptions {
     discard_stderr: bool,
-}
-
-/// Spawn a NodeJS subprocess to run the target
-///
-/// In case the fuzzer exits (crash or timeout), the default LibAFL signal handler calls
-/// `libc::exit`. When simply invoked with a `Command::spawn`, this didn't close the child
-/// process. Set a death signal with `set_pdeathsig` so that we send a SIGKILL to the child
-/// process when the fuzzer process terminates for any reason. This also pipes stdout and
-/// stdin of the child process for IPC.
-fn spawn_node_child(opts: SpawnNodeChildOptions) -> Result<Child> {
-    use nix::unistd;
-
-    let worker_script = find_worker_script()?;
-
-    let (in_read, in_write) = unistd::pipe()?;
-    let (out_read, out_write) = unistd::pipe()?;
-    let result = unsafe { unistd::fork() }?;
-    match result {
-        ForkResult::Parent { child } => {
-            drop(in_read);
-            drop(out_write);
-
-            Ok(Child {
-                pid: child,
-                stdout: PipeReader(out_read),
-                stdin: PipeWriter(in_write),
-            })
-        }
-        ForkResult::Child => {
-            use nix::sys::{prctl, signal::Signal};
-
-            prctl::set_pdeathsig(Signal::SIGKILL).expect("failed to set death signal");
-
-            drop(in_write);
-            drop(out_read);
-
-            let stderr = if opts.discard_stderr {
-                Stdio::null()
-            } else {
-                Stdio::inherit()
-            };
-
-            let err = Command::new("node")
-                .arg(worker_script)
-                .stdin(Stdio::from(in_read))
-                .stdout(Stdio::from(out_write))
-                .stderr(stderr)
-                .exec();
-            panic!("failed to spawn node subprocess: {}", err);
-        }
-    }
 }
 
 impl Worker {
@@ -194,48 +194,31 @@ impl Worker {
             Some(ShMemView::alloc(&mut provider)?)
         };
 
-        let proc = spawn_node_child(SpawnNodeChildOptions {
+        let proc = Child::spawn(SpawnNodeChildOptions {
             discard_stderr: !cfg!(debug_assertions),
         })?;
 
         let mut worker = Self {
             proc,
             shmem,
+            args,
             schema: None,
         };
 
-        worker.send(Message::Init(InitArgs {
-            mode: args.mode,
-            entrypoint: args.entrypoint,
-            schema_file: args.schema_file,
-            replay: args.replay,
-            shmem: worker.shmem.as_ref().map(|c| c.description()),
-            config_file: args.config_file,
-        }))?;
-
-        let ok = worker.recv()?;
-
-        if let Message::InitOk(schema) = ok {
-            worker.schema = schema;
-        } else {
-            bail!("expected Message::InitOk. received {:?}", ok)
-        }
+        worker.init_child_process()?;
 
         Ok(worker)
     }
 
     pub fn send(&mut self, msg: Message) -> io::Result<()> {
         let bytes = rmp_serde::to_vec_named(&msg).map_err(io::Error::other)?;
-
         self.proc.stdin.write_all(&bytes)?;
-
         Ok(())
     }
 
-    pub fn recv(&mut self) -> io::Result<Message> {
+    pub fn recv(&mut self) -> Result<Message> {
         loop {
-            let msg: Message =
-                rmp_serde::from_read(&mut self.proc.stdout).map_err(io::Error::other)?;
+            let msg = rmp_serde::from_read(&mut self.proc.stdout)?;
             if let Message::Log(msg) = msg {
                 log::info!("[worker] {}", msg);
             } else {
@@ -244,15 +227,26 @@ impl Worker {
         }
     }
 
-    pub fn invoke(&mut self, buf: &[u8]) -> Result<ExitCode> {
+    pub fn invoke(&mut self, buf: &[u8]) -> Result<ExitKind> {
+        let Ok(ok) = self.throwing_invoke(buf) else {
+            // something went wrong when invoking the input.
+            // restart the child process and mark this a crash.
+            self.restart_child_process()?;
+            return Ok(ExitKind::Crash);
+        };
+
+        Ok(if ok { ExitKind::Ok } else { ExitKind::Crash })
+    }
+
+    fn throwing_invoke(&mut self, buf: &[u8]) -> Result<bool> {
         let msg = Message::Invoke(InvokeArgs {
             bytes: buf.to_vec(),
         });
         self.send(msg)?;
         let ok = self.recv()?;
 
-        if let Message::InvokeOk(code) = ok {
-            Ok(code)
+        if let Message::InvokeOk(ok) = ok {
+            Ok(ok)
         } else {
             bail!("expected Message::InvokeOk(..). received {:?}", ok)
         }
@@ -267,23 +261,65 @@ impl Worker {
     }
 
     fn release_shmem(&mut self) -> Result<()> {
-        if let Some(shmem) = self.shmem_mut() {
-            let mut provider = StdShMemProvider::new()?;
-            provider.release_shmem(shmem);
+        if let Some(shmem) = self.shmem.take() {
+            drop(shmem)
         }
         Ok(())
     }
 
+    /// NOTE: child process must already be running
+    fn init_child_process(&mut self) -> Result<()> {
+        let args = InitArgs {
+            mode: self.args.mode.clone(),
+            entrypoint: self.args.entrypoint.clone(),
+            schema_file: self.args.schema_file.clone(),
+            replay: self.args.replay,
+            shmem: self.shmem.as_ref().map(|c| c.description()),
+            config_file: self.args.config_file.clone(),
+        };
+        self.send(Message::Init(args))?;
+
+        let ok = self.recv()?;
+
+        if let Message::InitOk(schema) = ok {
+            self.schema = schema;
+        } else {
+            bail!("expected Message::InitOk. received {:?}", ok)
+        }
+
+        Ok(())
+    }
+
+    fn restart_child_process(&mut self) -> Result<()> {
+        self.stop_child_process()?;
+
+        // This calls Drop on the old self.proc, which cleans up parent's end of pipes
+        self.proc = Child::spawn(SpawnNodeChildOptions {
+            discard_stderr: !cfg!(debug_assertions),
+        })?;
+
+        self.init_child_process()
+    }
+
     /// Try to terminate the process with IPC
-    pub fn terminate(&mut self) -> Result<()> {
+    fn stop_child_process(&mut self) -> Result<()> {
         if let Err(e) = self.send(Message::Terminate) {
             // assume the process is already dead if broken pipe
             if !matches!(e.kind(), io::ErrorKind::BrokenPipe) {
-                self.release_shmem()?;
-                bail!("{}", e)
+                return Err(e.into());
             }
-        };
-        self.proc.wait()?;
+        }
+
+        // need a wait to avoid zombies
+        // https://www.man7.org/linux/man-pages/man2/wait.2.html
+        _ = self.proc.wait()?;
+
+        Ok(())
+    }
+
+    /// Close the child process and release all resources
+    pub fn terminate(mut self) -> Result<()> {
+        self.stop_child_process()?;
         self.release_shmem()?;
         Ok(())
     }

packages/worker/bytes.ts
@@ -5,11 +5,11 @@ import assert from "node:assert";
 import type { Oracle } from "@railcar/support";
 import type { SharedExecutionData } from "@railcar/worker-sys";
 
-import { ExitKind, withOracle } from "./common.js";
+import { withOracle } from "./common.js";
 
 export class BytesExecutor {
-    _executor: (bytes: Uint8Array) => Promise<ExitKind> = (_) =>
-        Promise.resolve(ExitKind.Ok);
+    _executor: (bytes: Uint8Array) => Promise<boolean> = (_) =>
+        Promise.resolve(true);
     _shmem: SharedExecutionData | null = null;
 
     constructor(shmem: SharedExecutionData | null) {

packages/worker/common.ts
@@ -3,19 +3,16 @@
 import type { Oracle } from "@railcar/support";
 import type { SharedExecutionData } from "@railcar/worker-sys";
 
-export enum ExitKind {
-    Ok = 0,
-    Invalid = 1,
-    Crash = 2,
-    Abort = 3,
-}
-
+/**
+ * Run a fuzz target with an oracle. Return a boolean that is true
+ * if run was ok (no crash).
+ */
 export function withOracle<I>(
     fuzz: (_: I) => void | Promise<void>,
     oracle: Oracle,
     logError: boolean = false,
     shmem: SharedExecutionData | null = null,
-): (_: I) => Promise<ExitKind> {
+): (_: I) => Promise<boolean> {
     return async (data: I) => {
         try {
             // handles both sync and async functions
@@ -29,18 +26,17 @@ export function withOracle<I>(
                 if (shmem) {
                     shmem.setValid(true);
                 }
-                return ExitKind.Crash;
             } else {
                 if (shmem) {
                     shmem.setValid(false);
                 }
-                return ExitKind.Invalid;
             }
+            return false;
         }
 
         if (shmem) {
             shmem.setValid(true);
         }
-        return ExitKind.Ok;
+        return true;
     };
 }

packages/worker/graph.ts
@@ -15,12 +15,12 @@ import {
 import type { Oracle } from "@railcar/support";
 import type { SharedExecutionData } from "@railcar/worker-sys";
 
-import { ExitKind, withOracle } from "./common.js";
+import { withOracle } from "./common.js";
 import { ENABLE_DEBUG_INFO, ENABLE_HEAVY_ASSERTIONS } from "./config.js";
 
 export class GraphExecutor {
-    _executor: (graph: Graph) => Promise<ExitKind> = (_) =>
-        Promise.resolve(ExitKind.Ok);
+    _executor: (graph: Graph) => Promise<boolean> = (_) =>
+        Promise.resolve(true);
     _shmem: SharedExecutionData | null = null;
 
     constructor(shmem: SharedExecutionData | null) {
@@ -55,7 +55,7 @@ export class GraphExecutor {
         return schema;
     }
 
-    async execute(graph: Graph): Promise<ExitKind> {
+    async execute(graph: Graph): Promise<boolean> {
         if (ENABLE_DEBUG_INFO) {
             console.log(
                 JSON.stringify(

packages/worker/sequence.ts
@@ -10,7 +10,7 @@ import {
 } from "@railcar/inference";
 import type { SharedExecutionData } from "@railcar/worker-sys";
 
-import { ExitKind, withOracle } from "./common.js";
+import { withOracle } from "./common.js";
 import { FuzzedDataProvider, type Oracle } from "@railcar/support";
 import {
     ENABLE_DEBUG_INFO,
@@ -35,8 +35,7 @@ export type ApiCall = {
 export type ApiCallArg = { Constant: Type } | { Output: CallId } | "Missing";
 
 export class SequenceExecutor {
-    _executor: (seq: ApiSeq) => Promise<ExitKind> = (_) =>
-        Promise.resolve(ExitKind.Ok);
+    _executor: (seq: ApiSeq) => Promise<boolean> = (_) => Promise.resolve(true);
     _shmem: SharedExecutionData | null = null;
     _num_executed: number = 0;
 
@@ -72,7 +71,7 @@ export class SequenceExecutor {
         return schema;
     }
 
-    async execute(sequence: ApiSeq): Promise<ExitKind> {
+    async execute(sequence: ApiSeq): Promise<boolean> {
         this._num_executed = 0;
         const result = await this._executor(sequence);
         this._shmem?.setNumCallsExecuted(this._num_executed);

packages/worker/worker.ts
@@ -11,7 +11,6 @@ import type { Schema, Graph } from "@railcar/inference";
 import { makeRailcarConfig } from "@railcar/support";
 import { SharedExecutionData } from "@railcar/worker-sys";
 
-import type { ExitKind } from "./common.ts";
 import { codeCoverage } from "./instrument.js";
 import { BytesExecutor } from "./bytes.js";
 import { GraphExecutor } from "./graph.js";
@@ -46,7 +45,7 @@ type Message =
     | { Init: InitArgs }
     | { InitOk: Schema | null }
     | { Invoke: { bytes: Uint8Array } }
-    | { InvokeOk: ExitKind }
+    | { InvokeOk: boolean }
     | { Log: string }
     | "Terminate";
 
@@ -107,7 +106,7 @@ async function init(args: InitArgs): Promise<Schema | null> {
     }
 }
 
-async function invoke(bytes: Uint8Array): Promise<ExitKind> {
+async function invoke(bytes: Uint8Array): Promise<boolean> {
     assert(_executor !== null);
     if (_executor instanceof BytesExecutor) {
         return _executor.execute(bytes);
@@ -133,9 +132,9 @@ async function handleMessage(msg: Message) {
     }
 
     if ("Invoke" in msg) {
-        const kind = await invoke(msg.Invoke.bytes);
+        const ok = await invoke(msg.Invoke.bytes);
         send({
-            InvokeOk: kind,
+            InvokeOk: ok,
         });
         return;
     }

commit c3d8cefb2d6f9c4edabe1f107c6c9cc0c0b48497

fix(fuzzer): feedback for invalid input crashes
Fixes #15

Regression from 0b3329c1. We used to pass ExitKind::Ok to feedbacks even
for invalid crashes. After that commit we now pass ExitKind::Crash to
feedbacks for both valid and invalid crashes. This decouples validity
and exit kinds, updating feedbacks to directly read validity from
observer.

packages/cli/src/client.rs
@@ -129,7 +129,7 @@ fn bytes_client(
 
     // we don't want coverage feedback but we still want to count valid execution stats
     let mut feedback = StdFeedback::new(false, &observers);
-    let mut objective = UniqCrashFeedback::new(coverage);
+    let mut objective = UniqCrashFeedback::new(&observers);
 
     let mut state = state.unwrap_or_else(|| {
         StdState::new(
@@ -170,7 +170,7 @@ fn parametric_client(
     let coverage = &observers.0;
 
     let mut feedback = StdFeedback::new(true, &observers);
-    let mut objective = UniqCrashFeedback::new(coverage);
+    let mut objective = UniqCrashFeedback::new(&observers);
 
     let mut state = state.unwrap_or_else(|| {
         StdState::new(
@@ -213,7 +213,7 @@ fn graph_client(
     let coverage = &observers.0;
 
     let mut feedback = StdFeedback::new(true, &observers);
-    let mut objective = UniqCrashFeedback::new(coverage);
+    let mut objective = UniqCrashFeedback::new(&observers);
 
     let mut state = state.unwrap_or_else(|| {
         StdState::new(

packages/cli/src/ensemble.rs
@@ -64,7 +64,7 @@ fn run_client(
     let coverage = &observers.0;
 
     let mut feedback = StdFeedback::new(false, &observers);
-    let mut objective = UniqCrashFeedback::new(coverage);
+    let mut objective = UniqCrashFeedback::new(&observers);
 
     let mut state = state.unwrap_or_else(|| {
         StdState::new(

packages/fuzzer/src/feedback.rs
@@ -41,6 +41,22 @@ struct ValidInputsMetadata {
 
 libafl_bolts::impl_serdeany!(ValidInputsMetadata);
 
+impl ValidInputsMetadata {
+    const NAME: &'static str = "ValidInputsMetadata";
+
+    #[inline]
+    fn get<S: HasNamedMetadata>(state: &mut S) -> Result<&mut ValidInputsMetadata, libafl::Error> {
+        state.named_metadata_mut::<Self>(Self::NAME)
+    }
+
+    #[inline]
+    fn init<S: HasNamedMetadata>(state: &mut S) {
+        if !state.has_named_metadata::<Self>(Self::NAME) {
+            state.add_named_metadata(Self::NAME, Self::default());
+        }
+    }
+}
+
 /// Input metadata that labels each input as valid or invalid in the corpus.
 #[derive(Serialize, Deserialize, Debug, Default)]
 pub struct InputValidityMetadata {
@@ -88,7 +104,7 @@ where
 
         if is_interesting {
             let execs = {
-                let meta = state.named_metadata_mut::<ValidInputsMetadata>(self.name())?;
+                let meta = ValidInputsMetadata::get(state)?;
                 meta.num_valid_executions += 1;
                 meta.num_valid_executions
             };
@@ -130,7 +146,7 @@ where
         // if a new valid input is going into the corpus, record it in stats
         if is_valid {
             let count = {
-                let meta = state.named_metadata_mut::<ValidInputsMetadata>(self.name())?;
+                let meta = ValidInputsMetadata::get(state)?;
                 meta.num_valid_corpus += 1;
                 meta.num_valid_corpus
             };
@@ -159,7 +175,8 @@ impl Named for ValidityFeedback {
 
 impl<S: HasNamedMetadata> StateInitializer<S> for ValidityFeedback {
     fn init_state(&mut self, state: &mut S) -> Result<(), libafl::Error> {
-        state.add_named_metadata_checked(self.name(), ValidInputsMetadata::default())
+        ValidInputsMetadata::init(state);
+        Ok(())
     }
 }
 
@@ -320,6 +337,7 @@ pub struct StdFeedback {
     last_result: Option<bool>,
 
     // sub feedbacks
+    // TODO: what if we merged all of these into a big StdFeedback
     total_edges: TotalEdgesFeedback,
     validity: ValidityFeedback,
     total_coverage: CoverageFeedback,
@@ -357,19 +375,22 @@ where
         observers: &OT,
         exit_kind: &ExitKind,
     ) -> Result<bool, libafl::Error> {
-        if !matches!(exit_kind, ExitKind::Ok) {
-            // this is a crash that was deemed uninteresting by UniqCrashFeedback,
-            // no need to save this to the corpus
+        let is_valid = self
+            .validity
+            .is_interesting(state, manager, input, observers, exit_kind)?;
+
+        // must be ExitKind::Ok or invalid.
+        // valid crashes deemed uninteresting by UniqCrashFeedback should not
+        // go into the corpus.
+        // TODO: should we save invalid inputs to the corpus at all?
+        let should_consider = matches!(exit_kind, ExitKind::Ok) || !is_valid;
+        if !should_consider {
             self.last_result = Some(false);
             return Ok(false);
         }
 
         let is_new_total_coverage = self
             .total_coverage
-            .is_interesting(state, manager, input, observers, exit_kind)?;
-
-        let is_valid = self
-            .validity
             .is_interesting(state, manager, input, observers, exit_kind)?;
 
         let is_new_valid_coverage = if is_valid {
@@ -387,7 +408,6 @@ where
             .api_progress
             .is_interesting(state, manager, input, observers, exit_kind)?;
 
-        // TODO: should we save invalid inputs to the corpus at all?
         let is_interesting = if self.use_validity {
             is_new_total_coverage || is_new_valid_coverage
         } else {
@@ -463,15 +483,20 @@ impl Named for StdFeedback {
     }
 }
 
+/// Reports true if the input crashes, is valid, and covers new edges.
+/// Assumes this is an objective and used alongside StdFeedback.
 pub struct UniqCrashFeedback {
     coverage: CoverageFeedback,
+    validity_observer: Handle<ValidityObserver>,
     last_result: Option<bool>,
 }
 
 impl UniqCrashFeedback {
-    pub fn new(coverage_map: &CoverageObserver) -> Self {
+    pub fn new(observers: &Observers) -> Self {
+        let (coverage, (validity, _)) = observers;
         Self {
-            coverage: CoverageFeedback::with_name("CrashCoverage", coverage_map),
+            coverage: CoverageFeedback::with_name("CrashCoverage", coverage),
+            validity_observer: validity.handle(),
             last_result: None,
         }
     }
@@ -492,17 +517,48 @@ where
         observers: &OT,
         exit_kind: &ExitKind,
     ) -> Result<bool, libafl::Error> {
-        if !matches!(exit_kind, ExitKind::Crash | ExitKind::Timeout) {
+        if matches!(exit_kind, ExitKind::Ok) {
             self.last_result = Some(false);
             return Ok(false);
         }
+
+        let Some(validity_observer) = observers.get(&self.validity_observer) else {
+            return Err(libafl::Error::illegal_state(
+                "missing validity observer".to_string(),
+            ));
+        };
+
+        let is_valid = validity_observer.is_valid();
 
         let is_new_coverage = self
             .coverage
             .is_interesting(state, manager, input, observers, exit_kind)?;
 
-        self.last_result = Some(is_new_coverage);
-        Ok(is_new_coverage)
+        let is_interesting = is_valid && is_new_coverage;
+
+        // if this is interesting, we're not going to run feedbacks. Update stats
+        // that we would have updated in feedback otherwise.
+        if is_interesting {
+            let execs = {
+                let meta = ValidInputsMetadata::get(state)?;
+                meta.num_valid_executions += 1;
+                meta.num_valid_executions
+            };
+            manager.fire(
+                state,
+                EventWithStats::with_current_time(
+                    Event::UpdateUserStats {
+                        name: Cow::Borrowed("validexecs"),
+                        value: UserStats::new(UserStatsValue::Number(execs), AggregatorOps::Sum),
+                        phantom: PhantomData,
+                    },
+                    *state.executions(),
+                ),
+            )?
+        }
+
+        self.last_result = Some(is_interesting);
+        Ok(is_interesting)
     }
 
     fn last_result(&self) -> Result<bool, libafl::Error> {
@@ -528,6 +584,7 @@ where
     S: HasNamedMetadata,
 {
     fn init_state(&mut self, state: &mut S) -> Result<(), libafl::Error> {
+        ValidInputsMetadata::init(state);
         self.coverage.init_state(state)
     }
 }

packages/fuzzer/src/seq/mod.rs
@@ -70,8 +70,8 @@ fn client(
     let observers = make_observers(worker.shmem_mut().expect("must init shmem for fuzzing"));
     let coverage = &observers.0;
 
-    let mut objective = UniqCrashFeedback::new(coverage);
     let mut feedback = StdFeedback::new(false, &observers);
+    let mut objective = UniqCrashFeedback::new(&observers);
 
     let mut state = state.unwrap_or_else(|| {
         StdState::new(