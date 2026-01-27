9db559269c19710aac3ea094384350952deedf8f
Mayant Mukul <mayantm@proton.me>
2026-01-26 20:27:34 -0800

    chore: gen random schemas for more examples

--- scripts/common.ts
+++ scripts/common.ts
@@ -3,30 +3,30 @@ const PROJECTS = {
         bundle: "examples/fast-xml-parser/fxp.full.js",
         decl: "node_modules/fast-xml-parser/src/fxp.d.ts",
     },
-    // tslib: {
-    //     bundle: "node_modules/tslib/tslib.js",
-    //     decl: "node_modules/tslib/tslib.d.ts",
-    // },
-    // pako: {
-    //     bundle: "node_modules/pako/dist/pako.js",
-    //     decl: "node_modules/@types/pako/index.d.ts",
-    // },
-    // redux: {
-    //     bundle: "node_modules/redux/dist/redux.mjs",
-    //     decl: "node_modules/redux/dist/redux.d.ts",
-    // },
-    // jimp: {
-    //     bundle: "examples/jimp/jimp.browser.js",
-    //     decl: "node_modules/jimp/dist/commonjs/index.d.ts",
-    // },
-    // "jpeg-js": {
-    //     bundle: "examples/jpeg-js/jpeg-js.bundle.js",
-    //     decl: "node_modules/jpeg-js/index.d.ts",
-    // },
-    // "js-yaml": {
-    //     bundle: "node_modules/js-yaml/dist/js-yaml.js",
-    //     decl: "node_modules/@types/js-yaml/index.d.ts",
-    // },
+    tslib: {
+        bundle: "node_modules/tslib/tslib.js",
+        decl: "node_modules/tslib/tslib.d.ts",
+    },
+    pako: {
+        bundle: "node_modules/pako/dist/pako.js",
+        decl: "node_modules/@types/pako/index.d.ts",
+    },
+    redux: {
+        bundle: "node_modules/redux/dist/redux.mjs",
+        decl: "node_modules/redux/dist/redux.d.ts",
+    },
+    jimp: {
+        bundle: "examples/jimp/jimp.browser.js",
+        decl: "node_modules/jimp/dist/commonjs/index.d.ts",
+    },
+    "jpeg-js": {
+        bundle: "examples/jpeg-js/jpeg-js.bundle.js",
+        decl: "node_modules/jpeg-js/index.d.ts",
+    },
+    "js-yaml": {
+        bundle: "node_modules/js-yaml/dist/js-yaml.js",
+        decl: "node_modules/@types/js-yaml/index.d.ts",
+    },
     // lodash: {},
     // lit: {},
     // protobufjs: {},
