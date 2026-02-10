fe3dc058998e76b10ab13479c344672815451b07
Mayant Mukul <mayantm@proton.me>
2026-02-10 13:19:25 -0800

    feat(examples): remove private methods from xml2js

--- examples/xml2js/railcar.config.js
+++ examples/xml2js/railcar.config.js
@@ -1,4 +1,11 @@
 module.exports = {
     shouldInstrument: (f) => f.includes("xml2js"),
     isBug: () => false,
+    skipMethods: [
+        "Parser.processAsync",
+        "Parser.assignOrPush",
+        "Parser.EventEmitter",
+        "Parser.EventEmitterAsyncResource",
+        "Parser.init",
+    ]
 };
