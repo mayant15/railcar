53337d4a6bdf730ee658ed818462f1831360c485
Mayant Mukul <mayantm@proton.me>
2026-02-10 12:46:52 -0800

    feat(examples): remove private method from redux

--- examples/redux/railcar.config.js
+++ examples/redux/railcar.config.js
@@ -3,4 +3,7 @@ const { makeInvalidErrorMessageOracle } = require("@railcar/support");
 module.exports = {
     shouldInstrument: (f) => f.includes("redux"),
     isBug: makeInvalidErrorMessageOracle([]),
+    skipMethods: [
+        "__DO_NOT_USE__ActionTypes.PROBE_UNKNOWN_ACTION"
+    ]
 };
