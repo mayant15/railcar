966e3dedfde0271ff65bde26b51e46d2ab735809
Mayant Mukul <mayantm@proton.me>
2026-02-16 18:49:52 -0800

    test(infer): update tests for new defaults in reflection

--- packages/inference/reflection.test.ts
+++ packages/inference/reflection.test.ts
@@ -4,6 +4,7 @@ import { test, expect } from "bun:test";
 
 import type { Schema } from "./schema.js";
 import { loadSchemaFromObject } from "./reflection.js";
+import { Guess } from "./common.js";
 
 test("remove unexported endpoints", async () => {
     const schemaJson: Schema = {
@@ -18,11 +19,25 @@ test("remove unexported endpoints", async () => {
         schemaJson,
     );
 
-    expect("totallyLegit" in endpoints).toBeTrue();
-    expect("totallyLegit" in schema).toBeTrue();
-
     expect("missing" in endpoints).toBeFalse();
     expect("missing" in schema).toBeFalse();
 
     expect(Object.keys(endpoints).length).toBe(Object.keys(schema).length);
 });
+
+test("should not include functions not in schema", async () => {
+    const {schema, endpoints} = await loadSchemaFromObject("./test/lib.js", {})
+
+    expect(schema["totallyLegit"]).toBeUndefined();
+    expect(endpoints["totallyLegit"]).toBeUndefined();
+})
+
+test("should include functions not in schema if option set", async () => {
+    const {schema, endpoints} = await loadSchemaFromObject("./test/lib.js", {}, { skipEndpointsNotInSchema: false })
+    expect(schema["totallyLegit"]).toEqual({
+        args: [],
+        ret: Guess.any(),
+        callconv: "Free",
+    })
+    expect(endpoints["totallyLegit"]).toBeFunction();
+})
