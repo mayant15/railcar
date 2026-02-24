5ea8e20baa3b4029e747267f536f8d2f8a33cb1f
Mayant Mukul <mayantm@proton.me>
2026-02-24 14:56:57 -0800

    feat(scripts): print any guesses

--- scripts/analyze-api-surface.ts
+++ scripts/analyze-api-surface.ts
@@ -8,6 +8,7 @@ import type { Schema, SignatureGuess } from "@railcar/inference";
 import { getProjectNames, getProjectSpec, type Project } from "./common";
 
 const LATEX = false;
+let _isTypeScript = false;
 
 async function schema(project: string, kind: string): Promise<Schema> {
     const path = join("examples", project, `${kind}.json`);
@@ -34,11 +35,13 @@ function allAny(sig: SignatureGuess): boolean {
 function countNoInfoSignatures(schema: Schema): [number, number] {
     let total = 0;
     let any = 0;
-    for (const [_, guess] of Object.entries(schema)) {
+    for (const [name, guess] of Object.entries(schema)) {
         if (guess.builtin) continue;
         total += 1;
         if (allAny(guess)) {
-            // console.warn(" ", name);
+            if (_isTypeScript) {
+                console.warn(" ", name);
+            }
             any += 1;
         }
     }
@@ -87,8 +90,10 @@ async function analyze(project: Project): Promise<AnalyzeRow> {
     const [totalRandom, anyRandom] = countNoInfoSignatures(random);
     const [_totalSyntest, anySyntest] = [1, 0]; // countNoInfoSignatures(syntest);
 
-    // console.warn("typescript");
+    _isTypeScript = true;
+    console.warn("**", project, "**");
     const [totalTypescript, anyTypescript] = countNoInfoSignatures(typescript);
+    _isTypeScript = false;
 
     // assert(totalRandom === totalSyntest);
     assert(totalRandom === totalTypescript);
@@ -156,7 +161,9 @@ function printTable(data: AnalyzeRow[]) {
             Total: d.random.total,
             "Random # Any API": d.random.any,
             "Random % Any Types": d.random.anyTypeP.toFixed(1),
-            "TypeScript # Any API": d.typescript.known ? `${d.typescript.any} (${d.typescript.known} known)` : d.typescript.any,
+            "TypeScript # Any API": d.typescript.known
+                ? `${d.typescript.any} (${d.typescript.known} known)`
+                : d.typescript.any,
             "TypeScript % Any Types": d.typescript.anyTypeP.toFixed(1),
             // "SynTest # Any API": d.syntest.any,
             // "SynTest % Any Types": d.syntest.anyTypeP.toFixed(1),
