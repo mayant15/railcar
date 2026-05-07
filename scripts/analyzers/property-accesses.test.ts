8e4732852bb80c4d6b8261206d33ed040c903a1f
Mayant Mukul <mayantm@proton.me>
2026-05-06 19:37:44 -0700

    Fix property access test after refactor

--- scripts/analyzers/property-accesses.test.ts
+++ scripts/analyzers/property-accesses.test.ts
@@ -1,7 +1,7 @@
 import { expect, test } from "bun:test";
 import { countObjectPropertyAccesses } from "./property-accesses";
 
-test("no accesses", () => {
-    const count = countObjectPropertyAccesses("x");
+test("no accesses", async () => {
+    const count = await countObjectPropertyAccesses("x");
     expect(count).toBe(0);
 });
