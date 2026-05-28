394893c36283aee392cfe650cc25f386804e3727
Mayant Mukul <mayantm@proton.me>
2026-05-27 22:16:16 -0700

    Fix instrumentation for lit
    Largely the same as 18a9d2ee2c04885cc5f43b7f90834a94ab4738f9

--- examples/lit/railcar.config.js
+++ examples/lit/railcar.config.js
@@ -1,7 +1,7 @@
 const { makeInvalidErrorMessageOracle } = require("@railcar/support");
 
 module.exports = {
-    shouldInstrument: (f) => f.includes("lit"),
+    shouldInstrument: (f) => f.includes("node_modules/lit") || f.includes("node_modules/@lit"),
     isBug: makeInvalidErrorMessageOracle(["Cannot read properties"]),
     skipMethods: [
         "ReactiveElement.scheduleUpdate",
